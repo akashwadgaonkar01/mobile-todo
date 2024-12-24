783873956392b4578a45684b4c4c3de2572f08ca
Akash Wadgaonkar <akashbw01@gmail.com>
2024-12-24 09:24:47 +0530

    error handler added

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ app.use("/api/employee", require("./routes/employee.routes"));
 app.use("*", (req, res) => {
   res.status(404).json({ message: "resource not found" });
 });
+app.use((err, req, res, next) => {
+  if (err) {
+    console.log(err)
+    return res.status(500).json({message : "something went wrong"})
+  }
+})
 
 mongoose.connect(process.env.MONGO_URL);
 mongoose.connection.once("open", () => {
